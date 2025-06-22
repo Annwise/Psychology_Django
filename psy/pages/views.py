9fbe9c318a9a0c3df61759ee155a54990bd03bae
Annwise <shutova235@gmail.com>
2025-06-22 20:52:51 +0300

    psy

--- psy/pages/views.py
+++ psy/pages/views.py
@@ -1,5 +1,5 @@
-from django.http import HttpResponse
+from django.shortcuts import render
 
 
-def index(request):
-    return HttpResponse("Меня зовут Мария О., я психолог.")
+def home(request):
+    return render(request, 'pages/home.html')
