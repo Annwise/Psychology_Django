f73926d34c9fab103aad189b3ad5b8ff28a28741
unknown <valeriya9169566@gmaie.com>
2025-08-22 21:47:04 +0300

    new psy

--- psy/psy/urls.py
+++ psy/psy/urls.py
@@ -9,5 +9,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('pages.urls')),  # Главная страница
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
