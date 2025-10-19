9fed219f00e23338dae45bfb85c1e58959630a27
unknown <valeriya9169566@gmaie.com>
2025-10-19 21:01:29 +0300

    pages

--- psy/pages/forms.py
+++ psy/pages/forms.py
@@ -1,6 +1,7 @@
 from django import forms
 from .models import Booking
 
+
 class BookingForm(forms.ModelForm):
     class Meta:
         model = Booking
@@ -9,7 +10,12 @@ class BookingForm(forms.ModelForm):
             'name': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Ваше имя'}),
             'phone': forms.TextInput(attrs={'class': 'form-control', 'placeholder': 'Телефон'}),
             'email': forms.EmailInput(attrs={'class': 'form-control', 'placeholder': 'Email'}),
-            'service': forms.Select(attrs={'class': 'form-control'}),
+            'service': forms.Select(attrs={'class': 'form-control'}),  # ← Убери empty_label здесь
             'date': forms.DateTimeInput(attrs={'class': 'form-control', 'type': 'datetime-local'}),
             'message': forms.Textarea(attrs={'class': 'form-control', 'placeholder': 'Сообщение', 'rows': 4}),
         }
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.fields['service'].empty_label = "Выберите услугу"
+        self.fields['service'].queryset = self.fields['service'].queryset.filter(is_active=True)
--- psy/pages/models.py
+++ psy/pages/models.py
@@ -1,5 +1,7 @@
 from django.db import models
+
 from django.contrib.auth.models import User
+
 
 class Service(models.Model):
     title = models.CharField(max_length=200, verbose_name='Название услуги')
@@ -17,6 +19,7 @@ class Service(models.Model):
 
     def __str__(self):
         return self.title
+
 
 class Booking(models.Model):
     STATUS_CHOICES = [
